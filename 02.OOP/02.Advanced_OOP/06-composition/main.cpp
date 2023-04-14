66854b7520b9c3ef784b9e0e6024f2455a42c6fa
iforget <coding@algo.cn>
2023-04-14 20:48:48 +0800

    composition

---
+++ 02.OOP/02.Advanced_OOP/06-composition/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+#include <stdexcept>
+#include <cassert>
+
+#define PI (3.14159f)
+
+class LineSegment {
+public:
+    float length_;
+};
+
+class Circle {
+public:
+    Circle(const LineSegment& r) : radius_(r) {
+        if (r.length_ < 0) {
+            throw std::invalid_argument("raidus is negative");
+        }
+    }
+
+    float area() const {
+        return std::pow(radius_.length_, 2) * PI;
+    }
+private:
+    LineSegment radius_;
+};
+
+int main()
+{
+    LineSegment radius {3};
+    Circle circle {radius};
+    assert(std::fabs(28.28 - circle.area()) < 1.0);
+
+    std::cout << "Hello world" << std::endl;
+    return 0;
+}
+
