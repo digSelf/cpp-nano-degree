ac6ae9521453b28da3ac0baeae0e601c3ef2a437
iforget <coding@algo.cn>
2023-04-14 20:22:38 +0800

    inherited access modifers

---
+++ 02.OOP/02.Advanced_OOP/04-inherited-access-specifiers/main.cpp
@@ -0,0 +1,54 @@
+// This example demonstrates the privacy levels
+// between parent and child classes
+#include <iostream>
+#include <string>
+using std::string;
+
+class Vehicle {
+    public:
+        int wheels = 0;
+        string color = "blue";
+        
+        void Print() const
+        {
+            std::cout << "This " << color << " vehicle has " << wheels << " wheels!\n";
+        }
+};
+
+class Car : public Vehicle {
+    public:
+        bool sunroof = false;
+};
+
+class Bicycle : protected Vehicle {
+    public:
+        bool kickstand = true;
+        void Wheels(int w)
+        {
+            wheels = w;
+        }
+};
+
+class Scooter : private Vehicle {
+    public:
+        bool electric = false;
+        void Wheels(int w)
+        {
+            wheels = w;
+        }
+};
+
+int main() 
+{
+    Car car;
+    car.wheels = 4;
+
+    Bicycle bicycle;
+    bicycle.Wheels(2);
+    
+    // error: because the function member Print is protected. the users of child class could not access it directly.
+    // bicycle.Print();
+
+    Scooter scooter;
+    scooter.Wheels(1);
+};
