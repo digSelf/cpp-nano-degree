dc2b595485cf6a32069f856f482c0b45c105b521
iforget <coding@algo.cn>
2023-04-15 10:53:30 +0800

    friends

---
+++ 02.OOP/02.Advanced_OOP/08-friends/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cassert>
+
+class Rectangle;
+
+class Square {
+public:
+    Square(int s) : side_(s) {}
+
+private:
+    friend class Rectangle;
+    int side_;
+};
+
+class Rectangle {
+public:
+    Rectangle(const Square& sq) : width_(sq.side_), height_(sq.side_) {}
+
+    int area() const { return width_ * height_; }
+
+private:
+    int width_ = 0;
+    int height_ = 0;
+};
+
+int main()
+{
+    Square sq {4};
+    Rectangle rect {sq};
+    std::cout << rect.area() << std::endl;
+    return 0;
+}
+
