a22188a3b735af4793bfdfc5780c430915137e46
iforget <coding@algo.cn>
2023-04-17 15:06:30 +0800

    template

--- 02.OOP/02.Advanced_OOP/17-templates/main.cpp
+++ 02.OOP/02.Advanced_OOP/17-templates/main.cpp
@@ -4,6 +4,8 @@
 // create a generic function product that multiplies two parameters
 // generic code: it is a term for code that is independet of types.
 
+// a template is something you can parameterize with types or values.
+
 template <typename T>
 T multipli(T a, T b) {
     return a * b;
