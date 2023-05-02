a3a9e9494818f744d1c4cd97c8e8232725ef95d4
iforget <coding@algo.cn>
2023-05-02 16:38:10 +0800

    rvalue references

---
+++ 03.Memory-Management/05-resources-copy-policies/09-rvalue-references/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// rvalue is a temporary expression.
+int main()
+{
+    int i = 1; 
+    int j = 2; 
+    int k = i + j; 
+    int &&l = i + j; 
+
+    std::cout << "k = " << k << ", l = " << l << std::endl;
+
+    return 0; 
+}
