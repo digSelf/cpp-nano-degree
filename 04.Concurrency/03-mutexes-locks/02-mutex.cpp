4aad0ced6b26c554abd4d5820a068dbb66002dc0
iforget <coding@algo.cn>
2023-05-12 17:34:19 +0800

    mutex

---
+++ 04.Concurrency/03-mutexes-locks/02-mutex.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <thread>
+#include <future>
+#include <algorithm>
+#include <vector>
+#include <memory>
+#include <mutex>
+
+class Vehicle {
+public:
+    Vehicle(int id) : id_(id) {}
+
+private:
+    int id_;
+};
+
+class WaitingVehicles {
+public:
+    WaitingVehicles() : temp_vehicles_(0) {}
+
+    // getter and setter
+    void print_size() {
+        // std::cout << "#vehicles = " << temp_vehicles_ << std::endl;
+        mutex_.lock();
+        std::cout << "#vehicles = " << vehicles_.size() << std::endl;
+        mutex_.unlock();
+    }
+
+    // typical behavior methods
+    void push_back(Vehicle&& v) {
+        mutex_.lock();
+        vehicles_.push_back(std::move(v));  // data race would be cause an execpetion
+        mutex_.unlock();
+        // int old_num = temp_vehicles_;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));  // wait deliberately to expose the data race
+        // temp_vehicles_ += 1;
+    }
+private:
+    std::vector<Vehicle> vehicles_;
+    int temp_vehicles_;
+    std::mutex mutex_;
+};
+
+int main()
+{
+    std::shared_ptr<WaitingVehicles> queue = std::make_shared<WaitingVehicles>();
+    std::vector<std::future<void>> futrs;
+
+    for (int i = 0; i < 1000; ++i) {
+        futrs.emplace_back(std::async(std::launch::async, &WaitingVehicles::push_back, queue, std::move(Vehicle(i))));
+    }
+    
+    std::for_each(futrs.begin(), futrs.end(), [](std::future<void>& ftr){
+                    ftr.wait();
+                  });
+
+    queue->print_size();
+    return 0;
+}
+
