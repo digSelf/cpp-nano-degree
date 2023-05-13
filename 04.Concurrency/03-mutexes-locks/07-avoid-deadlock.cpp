375b1fa27deefa5e2aac310e9666d472db9e2194
iforget <coding@algo.cn>
2023-05-13 17:46:00 +0800

    lock_guard

---
+++ 04.Concurrency/03-mutexes-locks/07-avoid-deadlock.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <mutex>
+#include <stdexcept>
+#include <thread>
+#include <future>
+#include <vector>
+
+std::mutex g_mutex;
+double g_result;
+
+void print_result(int denom) {
+    std::cout << "for denom = " << denom << ", the result is " << g_result << std::endl;
+} 
+
+void divide_by_number(double num, double denom) {
+    try {
+        if (denom != 0) {
+            std::lock_guard<std::mutex> locker(g_mutex);
+            g_result = num / denom;
+            print_result(denom);
+        } else {
+            throw std::invalid_argument("Exception from thread: Division By 0!");
+        }
+    } catch (const std::invalid_argument& e) {
+        // notify the user about the exception and return
+        std::cout << e.what() << std::endl;
+        return;
+    }
+}
+
+int main()
+{
+    std::vector<std::future<void>> futrs;
+    double num = 42.0;
+    for (int i = -5; i < 5; ++i) {
+        futrs.emplace_back(std::async(std::launch::async, divide_by_number, num, i));
+    }
+
+    for (auto& f : futrs) {
+        f.wait();
+    }
+    return 0;
+}
+
